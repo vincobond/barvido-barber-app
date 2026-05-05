f2c7e7c10e16a882bb6b3fe7c365709ff3ce2095
vincobond <photizoobiorah@gmail.com>
2026-05-05 08:39:14 +0100

    yes chat
    yes chat

---
+++ dist/assets/skeleton-DhHElWa6.js
@@ -0,0 +1,1 @@
+import{j as s,f as a}from"./index-BVnkGM0E.js";function t({className:n,...e}){return s.jsx("div",{className:a("animate-pulse rounded-md bg-zinc-100 dark:bg-zinc-800",n),...e})}export{t as S};
